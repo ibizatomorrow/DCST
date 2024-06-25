69b953ee26ca612dfd585354565abb36538f0482
ibizatomorrow <1561754405@qq.com>
2024-06-25 22:54:33 +0800

    pemds7_upload

---
+++ STGNN/pemsd7/DCRNN/util.py
@@ -0,0 +1,79 @@
+import logging
+import numpy as np
+import os
+import pickle
+import scipy.sparse as sp
+import sys
+
+from scipy.sparse import linalg
+import json
+from pathlib import Path
+from datetime import datetime
+from itertools import repeat
+from collections import OrderedDict
+
+
+def load_graph_data(pkl_filename):
+    sensor_ids, sensor_id_to_ind, adj_mx = load_pickle(pkl_filename)
+    return sensor_ids, sensor_id_to_ind, adj_mx
+
+
+def load_pickle(pickle_file):
+    try:
+        with open(pickle_file, 'rb') as f:
+            pickle_data = pickle.load(f)
+    except UnicodeDecodeError as e:
+        with open(pickle_file, 'rb') as f:
+            pickle_data = pickle.load(f, encoding='latin1')
+    except Exception as e:
+        print('Unable to load data ', pickle_file, ':', e)
+        raise
+    return pickle_data
+
+def calculate_random_walk_matrix(adj_mx):
+    adj_mx = sp.coo_matrix(adj_mx)
+    d = np.array(adj_mx.sum(1))
+    d_inv = np.power(d, -1).flatten()
+    d_inv[np.isinf(d_inv)] = 0.
+    d_mat_inv = sp.diags(d_inv)
+    random_walk_mx = d_mat_inv.dot(adj_mx).tocoo()
+    return random_walk_mx
+
+def calculate_scaled_laplacian(adj_mx, lambda_max=2, undirected=True):
+    if undirected:
+        adj_mx = np.maximum.reduce([adj_mx, adj_mx.T])
+    L = calculate_normalized_laplacian(adj_mx)  # L is coo matrix
+    if lambda_max is None:
+        lambda_max, _ = linalg.eigsh(L, 1, which='LM')
+        lambda_max = lambda_max[0]
+    # L = sp.csr_matrix(L)
+    M, _ = L.shape
+    I = sp.identity(M, format='coo', dtype=L.dtype)
+    L = (2 / lambda_max * L) - I
+    # return L.astype(np.float32)
+    return L.tocoo()
+
+def calculate_normalized_laplacian(adj):
+    """
+    # L = D^-1/2 (D-A) D^-1/2 = I - D^-1/2 A D^-1/2
+    # D = diag(A 1)
+    :param adj:
+    :return:
+    """
+    adj = sp.coo_matrix(adj)
+    d = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(d, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    normalized_laplacian = sp.eye(adj.shape[0]) - adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+    return normalized_laplacian
+
+
+def read_json(fname):
+    with fname.open('rt') as handle:
+        return json.load(handle, object_hook=OrderedDict)
+
+
+def write_json(content, fname):
+    with fname.open('wt') as handle:
+        json.dump(content, handle, indent=4, sort_keys=False)
