aab6676e2478c09703b79037ec3b913bb38ac5dd
ibizatomorrow <1561754405@qq.com>
2025-07-03 13:46:57 +0800

    init

---
+++ STGNN/pemsbay/MTGNN/util.py
@@ -0,0 +1,20 @@
+import pickle
+import numpy as np
+import os
+import scipy.sparse as sp
+import torch
+from scipy.sparse import linalg
+from torch.autograd import Variable
+
+
+def load_adj(pickle_file):
+    try:
+        with open(pickle_file, 'rb') as f:
+            pickle_data = pickle.load(f)
+    except UnicodeDecodeError as e:
+        with open(pickle_file, 'rb') as f:
+            pickle_data = pickle.load(f, encoding='latin1')
+    except Exception as e:
+        print('Unable to load data ', pickle_file, ':', e)
+        raise
+    return pickle_data
